dddccceb57ee99d939f6541e4d4f3de4d92c9ad7
jupiter-maker <2572778568@qq.com>
2020-07-07 22:33:20 +0800

    登录表单及请求

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Message } from 'element-ui'
+import {Form,FormItem} from 'element-ui'
+import {Input} from 'element-ui'
+//导入弹框提示组件
+import {message} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message=Message
+
